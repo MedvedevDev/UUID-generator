32fde960027070b4a5edf2dc682eef5e040619db
Artem Medvedev <a95.medvedev@gmail.com>
2023-03-11 22:05:53 +0200

    UUID generator

--- uuid-gen.js
+++ uuid-gen.js
@@ -8,7 +8,6 @@
 /**
  * crypto.getRandomValue() guarantees to be cryptographically secure and also enjoys ubiquitous support from the browsers
  */
-
 function uuidv4() {
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0]
